36349d16350b26c4c58fd3ccf1554685f960745e
Ahatasham Arman <ahatasham5@gmail.com>
2025-09-29 22:54:40 +0600

    chapter 6

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,12 +1,8 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-console.log('DEBUG: fonts.ts file is being loaded');
-
 export const inter = Inter({ subsets: ['latin'] });
 
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
-
-console.log('DEBUG: lusitana font exported successfully:', !!lusitana);
